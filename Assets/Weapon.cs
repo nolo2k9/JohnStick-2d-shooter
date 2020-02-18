31abd33770a6bdd7c9d341bd2663edbbcad6be61
Keith Nolan <g00351932@gmit.ie>
2020-02-18 17:14:04 +0000

    Added weapon mouse movement, added a simple background

---
+++ Assets/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+   
+    // Update is called once per frame
+   private void Update()
+    {
+        //Callculating the the current mouse position to the current weapon position
+        //Input.mouse position returns a screenpoint
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = rotation;
+    }
+}
