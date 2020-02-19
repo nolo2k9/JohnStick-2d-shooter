6fbe0a01550056c5902a1f5142d1d409610419ba
Keith Nolan <g00351932@gmit.ie>
2020-02-19 13:03:17 +0000

    Got player shooting

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    //variable to handle what progectile to spawn
+    public GameObject projectile;
+    //from where does the bullet coem from
+    public Transform shotPoint;
+    //How much time should be taken between shots
+    public float timeBetweenShots;
+    //time to shoot another bullet
+    private float shotTime;
+
+
    
     // Update is called once per frame
    private void Update()
@@ -14,5 +24,19 @@ public class Weapon : MonoBehaviour
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = rotation;
+
+        //if the left mouse button is pressed
+        if(Input.GetMouseButton(0)){
+            
+            //if the current time in the game is greater than the shot time
+            if (Time.time >= shotTime)
+            {
+                //shoot projectile
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                //recalculate shot time current time + time between shots
+                shotTime = Time.time + timeBetweenShots;
+            }
+
+        }
     }
 }
