2b02f7e75c738acd92b439b624e196cc6e010f6a
Keith Nolan <g00351932@gmit.ie>
2020-03-12 18:41:54 +0000

    Added UI elements, Main menu and navigation

---
+++ Assets/SceneChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+
+    private Animator transitionAnim;
+
+    private void Start()
+    {
+        transitionAnim = GetComponent<Animator>();
+    }
+
+    public void LoadScene(string sceneName) {
+        StartCoroutine(Transition(sceneName));
+    }
+
+    IEnumerator Transition(string sceneName) {
+        transitionAnim.SetTrigger("tran2");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
+
+    
+
