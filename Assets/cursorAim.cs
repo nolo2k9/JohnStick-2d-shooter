31faae4784518529109b07158265fcf14994e8d5
Keith Nolan <g00351932@gmit.ie>
2020-03-18 19:23:08 +0000

    Fixed some bugs made a master volume slider

--- Assets/cursorAim.cs
+++ Assets/cursorAim.cs
@@ -8,7 +8,7 @@ public class cursorAim : MonoBehaviour
     //hide cursor
     private void Start()
     {
-        Cursor.visible = false;
+        
     }
   
 
---
+++ Assets/settings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class settings : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+}
